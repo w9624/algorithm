b585b16890fa30a8425f3d5f22d128e69becb05c
w9624 <wl7426@163.com>
2018-06-12 12:17:19 +0800

    初始化

--- src/main/java/sort/BobbleSort.java
+++ src/main/java/sort/BobbleSort.java
@@ -10,9 +10,10 @@ public class BobbleSort {
 	/**
 	 * ºÚµ•√∞≈›≈≈–Ú
 	 * @param arr
+	 * 
 	 */
 	public static void bobble(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length - 2; i++) {
 			for (int j = 0; j < arr.length - i - 1; j++) {
 				if (arr[j] > arr[j + 1]) {
 					int k = arr[j];
